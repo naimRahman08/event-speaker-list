95c6e03479996463f42fc8a074906ef20fffd790
Naim Rahman <naim.life@gmail.com>
2023-01-29 18:03:11 +0100

    feat: Add project structure

---
+++ src/app/modules/speakers/speakers.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpeakersRoutingModule } from './speakers-routing.module';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+
+@NgModule({
+  declarations: [
+    ListComponent,
+    DetailComponent
+  ],
+  imports: [
+    CommonModule,
+    SpeakersRoutingModule
+  ]
+})
+export class SpeakersModule { }
