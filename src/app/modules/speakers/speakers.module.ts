4f9f370ff9b3664ed476b3166433c7aefe72f7de
Naim Rahman <naim.life@gmail.com>
2023-01-29 19:07:18 +0100

    fix: List cmponent loading path

--- src/app/modules/speakers/speakers.module.ts
+++ src/app/modules/speakers/speakers.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SpeakersRoutingModule } from './speakers-routing.module';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './pages/list/list.component';
 import { DetailComponent } from './pages/detail/detail.component';
 
 
