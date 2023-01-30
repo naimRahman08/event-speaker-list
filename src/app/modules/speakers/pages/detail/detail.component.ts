0bbbeefc1b8cb9600ba127bc6be464e4f58ec1ca
Naim Rahman <naim.life@gmail.com>
2023-01-30 02:49:45 +0100

    feat: Add detail view for speaker

--- src/app/modules/speakers/pages/detail/detail.component.ts
+++ src/app/modules/speakers/pages/detail/detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  
+  speaker!: any;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap
+      .pipe(map(() => window.history.state))
+      .subscribe(response => {
+        this.speaker = response.speaker;
+      });
+  }
 
 }
